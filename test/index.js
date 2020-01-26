2fa1ee46ab5ec3bd3062e134db760c4b918fcfe1
Anthony Liu <a@anthony.ai>
2020-01-25 17:53:00 -0800

    Add a test for the main export.

--- test/index.js
+++ test/index.js
@@ -1,12 +1,56 @@
 const assert = require('assert');
 const { describe, it } = require('cafezinho');
-const { type, toString, addMoat, getLabeledPiece } = require('../lib');
+const {
+  addMoat,
+  getAllLabels,
+  getLabeledPiece,
+  toString,
+  type,
+} = require('../lib');
 
 function deepEquals(a, b) {
   return JSON.stringify(a) === JSON.stringify(b);
 }
 
 describe('Blokus', () => {
+  describe('#getAllLabels()', () => {
+    it('should work for a piece with redundant symmetries', () => {
+      const actual = getAllLabels([
+        [type.block, type.block, type.empty],
+        [type.empty, type.block, type.empty],
+        [type.empty, type.block, type.block],
+      ]);
+      const expected = [
+        [
+          [type.corner, type.edge, type.edge, type.corner, type.empty ],
+          [type.edge, type.block, type.block, type.edge, type.empty ],
+          [type.corner, type.edge, type.block, type.edge, type.corner ],
+          [type.empty, type.edge, type.block, type.block, type.edge ],
+          [type.empty, type.corner, type.edge, type.edge, type.corner ]
+        ], [
+          [type.empty, type.empty, type.corner, type.edge, type.corner],
+          [type.corner, type.edge, type.edge, type.block, type.edge],
+          [type.edge, type.block, type.block, type.block, type.edge],
+          [type.edge, type.block, type.edge, type.edge, type.corner],
+          [type.corner, type.edge, type.corner, type.empty, type.empty]
+        ], [
+          [type.empty, type.corner, type.edge, type.edge, type.corner],
+          [type.empty, type.edge, type.block, type.block, type.edge],
+          [type.corner, type.edge, type.block, type.edge, type.corner],
+          [type.edge, type.block, type.block, type.edge, type.empty],
+          [type.corner, type.edge, type.edge, type.corner, type.empty]
+        ], [
+          [type.corner, type.edge, type.corner, type.empty, type.empty],
+          [type.edge, type.block, type.edge, type.edge, type.corner],
+          [type.edge, type.block, type.block, type.block, type.edge],
+          [type.corner, type.edge, type.edge, type.block, type.edge],
+          [type.empty, type.empty, type.corner, type.edge, type.corner]
+        ]
+      ];
+      assert(deepEquals(actual, expected));
+    })
+  });
+
   describe('#getLabeledPiece()', () => {
     it('should work for a simple piece', () => {
       const actual = getLabeledPiece([[type.block]]);
@@ -47,7 +91,6 @@ describe('Blokus', () => {
         [type.edge, type.block, type.block, type.edge],
         [type.corner, type.edge, type.edge, type.corner]
       ];
-      console.log(toString(actual));
       assert(deepEquals(actual, expected));
     });
   });
