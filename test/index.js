a01d8e42a4ae92eb8eb0492ecefb0cafcd6376d1
Anthony Liu <a@anthony.ai>
2020-01-25 17:34:24 -0800

    Get the symmetries for a piece.

--- test/index.js
+++ test/index.js
@@ -1,12 +1,57 @@
 const assert = require('assert');
 const { describe, it } = require('cafezinho');
-const { type, addMoat } = require('../lib');
+const { type, toString, addMoat, getLabeledPiece } = require('../lib');
 
 function deepEquals(a, b) {
   return JSON.stringify(a) === JSON.stringify(b);
 }
 
 describe('Blokus', () => {
+  describe('#getLabeledPiece()', () => {
+    it('should work for a simple piece', () => {
+      const actual = getLabeledPiece([[type.block]]);
+      const expected = [
+        [type.corner, type.edge, type.corner],
+        [type.edge, type.block, type.edge],
+        [type.corner, type.edge, type.corner]
+      ];
+      assert(deepEquals(actual, expected));
+    });
+
+    it('should work for a square piece', () => {
+      const actual = getLabeledPiece([
+        [type.block, type.block, type.block],
+        [type.block, type.block, type.block],
+        [type.block, type.block, type.block]
+      ]);
+      const expected = [
+        [type.corner, type.edge, type.edge, type.edge, type.corner],
+        [type.edge, type.block, type.block, type.block, type.edge],
+        [type.edge, type.block, type.block, type.block, type.edge],
+        [type.edge, type.block, type.block, type.block, type.edge],
+        [type.corner, type.edge, type.edge, type.edge, type.corner]
+      ];
+      assert(deepEquals(actual, expected));
+    });
+
+    it('should work for a concave piece', () => {
+      const actual = getLabeledPiece([
+        [type.block, type.edge],
+        [type.block, type.edge],
+        [type.block, type.block]
+      ]);
+      const expected = [
+        [type.corner, type.edge, type.corner, type.empty],
+        [type.edge, type.block, type.edge, type.empty],
+        [type.edge, type.block, type.edge, type.corner],
+        [type.edge, type.block, type.block, type.edge],
+        [type.corner, type.edge, type.edge, type.corner]
+      ];
+      console.log(toString(actual));
+      assert(deepEquals(actual, expected));
+    });
+  });
+
   describe('#addMoat()', () => {
     it('should work for a simple piece', () => {
       const actual = addMoat([[type.block]]);
